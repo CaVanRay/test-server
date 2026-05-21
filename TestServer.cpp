f029bb388dbe58dfdc6cf393711b67fa065073bb
Cavan Theiss <cavanrt@gmail.com>
2026-05-20 19:15:54 -0500

    Update TestServer.cpp

--- TestServer.cpp
+++ TestServer.cpp
@@ -1,1 +1,16 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT 0
+#include "httplib.h"
+#include <iostream>
 
+int main() {
+    httplib::Server server;
+
+    server.Get("/hello", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("Hello from C++!", "text/plain");
+    });
+
+    std::cout << "Server running on http://localhost:8080" << std::endl;
+    server.listen("localhost", 8080);
+
+    return 0;
+}
